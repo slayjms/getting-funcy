8c43a4739f8f76cab1065baa1701e01742c50190
Slay James <slayjms@yahoo.com>
2024-09-26 10:00:25 -0500

    getting-funcy

---
+++ README.md
@@ -0,0 +1,47 @@
+------
+
+## **Getting Funcy**
+
+### **Setup**
+
+1. Open your terminal on your computer.
+2. If you haven't already, install the Mocha testing suite using npm install -g mocha.
+3. Run a command `npm test` in your terminal to initiate the testing process.
+
+### **Workflow**
+
+You'll be primarily working in a file named `main.test.js`. Follow the instructions outlined in the Tasks section below to create and utilize global variables and functions. Pay attention to the feedback provided in the terminal to guide your progress!
+
+### **Guidelines**
+
+- Functions act as specialized tools that can perform specific tasks or calculations.
+- Avoid directly modifying variable values; instead, utilize functions to manipulate them.
+
+### **Tasks**
+
+Let's begin by setting up some numerical variables that we'll use for calculations.
+
+#### **Variables**
+
+- Create a global variable named `a` and assign it a number value of your choice.
+- Define another global variable named `b` and assign it a different value.
+- Establish a global variable named `c` and set it initially to `0`.
+- Define a global variable named `name`and set it equal to a string of your first name.
+- Define a global variable named `lastName`and set it equal to a string of your last name.
+- Define a global variable named `trafficLight` and set it equal to 'red'.
+
+
+Now, let's develop some functions to perform mathematical operations with these variables!
+
+#### **Functions**
+
+- Write a function named `addition` to add the values of `a` and `b`, storing the result in `c`.
+- Write a function called `subtraction` to subtract the value of `b` from `a`, storing the result in `c`.
+- Write a function named `multiplication` to multiply the values of `a` and `b`, storing the result in `c`.
+- Write a function named `division` to divide the value of `a` by `b`, storing the result in `c`.
+- Write a function named `incrementA` to increase the value of `a` by `1`.
+- Write a function called `decrementB` to decrease the value of `b` by `1`.
+- Write a function called `fullName` that sets the variable `name` with a space and the variable `lastName` together.
+- Write a function called `lightChange` that sets the variable `trafficLight` equal to 'green'.
+
+---
---
+++ main.test.js
@@ -0,0 +1,154 @@
+/**********************************
+ * YOUR CODE GOES HERE THIS TIME! *
+ **********************************/
+let a = 3
+let b = 5
+let c = 0
+let name = "Slay"
+let lastName = "James"
+let trafficLight = "red"
+
+function addition(){
+  c = a + b
+}
+function subtraction(){
+  c = a - b
+}
+function multiplication(){
+  c = a * b
+}
+function division(){
+  c = a/b
+}
+function incrementA(){
+  a = a + 1
+}
+function decrementB(){
+  b = b - 1
+}
+function fullName(name, lastName){
+  return name + " " + lastName
+}
+
+
+
+/*********************************************************************************************************************
+
+ * CODE BELOW HERE IS FOR INTERNAL USE ONLY! IT ENSURES THAT THE TESTING *
+ * CAN READ AND EVALUATE YOUR CODE. *
+ * DON'T CHANGE ANYTHING BELOW HERE, PLEASE! *
+
+ *********************************************************************************************************************/
+
+if (typeof a === 'undefined') {
+  a = undefined;
+}
+
+if (typeof b === 'undefined') {
+  b = undefined;
+}
+
+if (typeof c === 'undefined') {
+  c = undefined;
+}
+
+if (typeof name === 'undefined') {
+  name = undefined;
+}
+
+if (typeof lastName === 'undefined') {
+  lastName = undefined;
+}
+
+if (typeof trafficLight === 'undefined') {
+  trafficLight = undefined;
+}
+
+if (typeof addition === 'undefined') {
+  addition = undefined;
+}
+
+if (typeof subtraction === 'undefined') {
+  subtraction = undefined;
+}
+
+if (typeof multiplication === 'undefined') {
+  multiplication = undefined;
+}
+
+if (typeof division === 'undefined') {
+  division = undefined;
+}
+
+if (typeof incrementA === 'undefined') {
+  incrementA = undefined;
+}
+
+if (typeof decrementB === 'undefined') {
+  decrementB = undefined;
+}
+
+if (typeof fullName === 'undefined') {
+  fullName = undefined;
+}
+
+if (typeof lightChange === 'undefined') {
+  lightChange = undefined;
+}
+
+const assert = require('assert');
+
+// Mocha tests
+describe('Getting Funcy Tests', () => {
+  // Test addition function
+  it('should correctly add a and b', () => {
+      addition();
+      assert.strictEqual(c, a + b);
+  });
+
+  // Test subtraction function
+  it('should correctly subtract b from a', () => {
+      subtraction();
+      assert.strictEqual(c, a - b);
+  });
+
+  // Test multiplication function
+  it('should correctly multiply a and b', () => {
+      multiplication();
+      assert.strictEqual(c, a * b);
+  });
+
+  // Test division function
+  it('should correctly divide a by b', () => {
+      division();
+      assert.strictEqual(c, a / b);
+  });
+
+  // Test incrementA function
+  it('should increment a by 1', () => {
+      let val = a
+      incrementA();
+      assert.strictEqual(a, val + 1);
+  });
+
+  // Test decrementB function
+  it('should decrement b by 1', () => {
+      let val = b
+      decrementB();
+      assert.strictEqual(b, val - 1);
+  });
+
+  // Test fullName function
+  it('should set name as first name and last name combined', () => {
+      let firstName = name
+      fullName();
+      
+      assert.strictEqual(name, firstName + ' ' + lastName);
+  });
+
+  // Test lightChange function
+  it('should set trafficLight to green', () => {
+      lightChange();
+      assert.strictEqual(trafficLight, "green");
+  });
+});
