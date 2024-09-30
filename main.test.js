07d0cc1df35592a7a25a08ec2f8ba35f91b0efe8
Slay James <slayjms@yahoo.com>
2024-09-29 22:15:25 -0500

    getting-funcy

--- main.test.js
+++ main.test.js
@@ -4,8 +4,8 @@
 let a = 3
 let b = 5
 let c = 0
-let name = "Slay"
-let lastName = "James"
+let firstName = 'Slay'
+let lastName = 'James'
 let trafficLight = "red"
 
 function addition(){
@@ -27,8 +27,9 @@ function decrementB(){
   b = b - 1
 }
 function fullName(name, lastName){
-  return name + " " + lastName
+   firstName + '' + lastName
 }
+function lightChange()
 
 
 
